8e5535fd716fb5365f3717171dcbf3f9ce067687
Woody Goodricke <github@andrewgoodricke.com>
2015-09-14 15:44:29 -0300

    Params to plugin: Allow any to be passed through
    Added more tests

--- test/index.js
+++ test/index.js
@@ -4,31 +4,32 @@ require('harmonize')();
 var expect        = require('chai').expect
 var equal         = require('assert-dir-equal');
 var Metalsmith    = require('metalsmith');
+var templates     = require('metalsmith-templates');
 var json_to_files = require('../lib');
 
 
-describe('metalsmith-json-to-files basic', function(){
+describe('metalsmith-json-to-files basic', function (){
     
     var test_path = 'test/fixtures/basic';
     
-    it('should fail without source_path', function (done){
+    it('should fail without source_path', function (done) {
 
         new Metalsmith(test_path)
         .use(json_to_files())
-        .build(function(err){
+        .build(function (err) {
             
             expect(err).to.be.an('error');
             done();
         });
     });
 
-    it('should do standard file copying', function (done){
+    it('should do standard file copying', function (done) {
 
         new Metalsmith(test_path)
         .use(json_to_files({
             source_path: '../json/'
         }))
-        .build(function(err){
+        .build(function (err) {
             if (err){ return done(err); }
             
             equal(test_path + '/expected', test_path + '/build');
@@ -39,17 +40,61 @@ describe('metalsmith-json-to-files basic', function(){
 });
 
 
-describe('metalsmith-json-to-files file generation', function(){
+describe('metalsmith-json-to-files file generation', function () {
     
     var test_path = 'test/fixtures/file_generation';
     
-    it('should do basic file generation', function (done){
+    it('should do basic file generation', function (done) {
 
         new Metalsmith(test_path)
         .use(json_to_files({
             source_path: '../json/'
         }))
-        .build(function(err){
+        .build(function (err) {
+            if (err){ return done(err); }
+            
+            equal(test_path + '/expected', test_path + '/build');
+            done();
+        });
+    });
+});
+
+
+describe('metalsmith-json-to-files file generation with permalinks', function () {
+    
+    var test_path = 'test/fixtures/file_generation_permalinks';
+    
+    it('should do basic file generation', function (done) {
+
+        new Metalsmith(test_path)
+        .use(json_to_files({
+            source_path: '../json/'
+        }))
+        .build(function (err) {
+            if (err){ return done(err); }
+            
+            equal(test_path + '/expected', test_path + '/build');
+            done();
+        });
+    });
+});
+
+
+describe('metalsmith-json-to-files file generation with templates', function () {
+    
+    var test_path = 'test/fixtures/hbs_templates';
+    
+    it('should do basic file generation', function (done) {
+
+        new Metalsmith(test_path)
+        .use(json_to_files({
+            source_path: '../json/'
+        }))
+        .use(templates({
+            engine     : 'handlebars'
+          , directory: 'templates'
+        }))
+        .build(function (err) {
             if (err){ return done(err); }
             
             equal(test_path + '/expected', test_path + '/build');
@@ -61,4 +106,5 @@ describe('metalsmith-json-to-files file generation', function(){
 
 // TODO: Test missing json file
 // TODO: Test filename not being able to be generated
+// TODO: Test Permalinks
 // TODO: Test with handlebars templates
